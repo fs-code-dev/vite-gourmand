8a79282214f7062a0331b1c08491665fde55f530
fs-code-dev <bfrida7@gmail.com>
2026-05-21 10:34:35 +0200

    Livrable ECF Final - Build OK

--- src/Router/allRoutes.js
+++ src/Router/allRoutes.js
@@ -3,33 +3,33 @@ import Route from "./Route.js";
 // Routes de l'application
 export const allRoutes = [
   // Accueil
-  new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/404", "Page non trouvée", "/pages/404.html"),
+  new Route("/", "Accueil", "/src/pages/home.html"),
+  new Route("/404", "Page non trouvée", "/src/pages/404.html"),
   
   // Pages d'informations
-  new Route("/cgv", "Conditions Générales de Vente", "/pages/cgv.html"),
-  new Route("/mentions", "Mentions légales", "/pages/mentions.html"),
-  new Route("/contact", "Formulaire de contact", "/pages/contact.html"),
+  new Route("/cgv", "Conditions Générales de Vente", "/src/pages/cgv.html"),
+  new Route("/mentions", "Mentions légales", "/src/pages/mentions.html"),
+  new Route("/contact", "Formulaire de contact", "/src/pages/contact.html"),
 
   // Menus et Commandes
-  new Route("/menus", "Menus", "/pages/menus.html", "/src/assets/js/menus.js"),
-  new Route("/menusdetail", "Détail du menu", "/pages/menus-detail.html", "/src/assets/js/menus-detail.js"),
-  new Route("/order", "Passer une commande", "/pages/order.html"),
+  new Route("/menus", "Menus", "/src/pages/menus.html", "/src/assets/js/menus.js"),
+  new Route("/menusdetail", "Détail du menu", "/src/pages/menus-detail.html", "/src/assets/js/menus-detail.js"),
+  new Route("/order", "Passer une commande", "/src/pages/order.html"),
 
   // AUTH (Authentification et Espace Client)
-  new Route("/signin", "Se connecter", "/pages/users/signin.html", "/src/assets/js/auth.js"),
-  new Route("/signup", "S'inscrire", "/pages/users/signup.html", "/src/assets/js/auth.js"),
-  new Route("/userSpace", "Espace utilisateur", "/pages/users/userSpace.html", "/src/assets/js/userSpace.js"),
-  new Route("/editPassword", "Modifier mon mot de passe", "/pages/users/editPassword.html", "/src/assets/js/auth.js"),
-  new Route("/modifyUserInfo", "Modifier mes informations", "/pages/users/modifyUserInfo.html", "/src/assets/js/auth.js"),
+  new Route("/signin", "Se connecter", "/src/pages/users/signin.html", "/src/assets/js/auth.js"),
+  new Route("/signup", "S'inscrire", "/src/pages/users/signup.html", "/src/assets/js/auth.js"),
+  new Route("/userSpace", "Espace utilisateur", "/src/pages/users/userSpace.html", "/src/assets/js/userSpace.js"),
+  new Route("/editPassword", "Modifier mon mot de passe", "/src/pages/users/editPassword.html", "/src/assets/js/auth.js"),
+  new Route("/modifyUserInfo", "Modifier mes informations", "/src/pages/users/modifyUserInfo.html", "/src/assets/js/auth.js"),
   
   // ESPACE EMPLOYÉ
-  new Route("/employeSpace", "Espace employé", "/pages/employe/employeSpace.html", "/src/assets/js/materiel.js"),
+  new Route("/employeSpace", "Espace employé", "/src/pages/employe/employeSpace.html", "/src/assets/js/materiel.js"),
 
   // ADMIN
-  new Route("/adminSpace", "Espace administrateur", "/pages/admin/adminSpace.html"),
-  new Route("/admin/users", "Gestion des employés", "/pages/admin/users.html", "/src/assets/js/users.js"),
-  new Route("/admin/stats", "Statistiques", "/pages/admin/stats.html", "/src/assets/js/stats.js"),
+  new Route("/adminSpace", "Espace administrateur", "/src/pages/admin/adminSpace.html"),
+  new Route("/admin/users", "Gestion des employés", "/src/pages/admin/users.html", "/src/assets/js/users.js"),
+  new Route("/admin/stats", "Statistiques", "/src/pages/admin/stats.html", "/src/assets/js/stats.js"),
 ];
  
 // Le titre s'affiche comme ceci : Route.titre - websiteName
